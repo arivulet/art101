c93285a91a224c0d079dc6bae7baef542e23d557
River K <nightwingsking25@gmail.com>
2025-04-21 18:20:43 -0700

    lab4
    lab4!

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,41 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Connect 4
+// A pseduocoding assignment
+// Author : River Kinley & Marisa Wong
+// Date: April 20 2025
+
+//Four in a Row
+
+// Setting up
+// Assign Player 1 with Red, And Player 2 with Yellow
+// Print an empty 6-row by 7-column grid with empty values
+// Decide which player goes first.
+// Show the current grid to players after every move.
+// Prompt current player to choose a column (0 to 6).
+// If the chosen column is full:
+// Print "Column full, choose another.”
+// Update the board
+// If the chosen column is NOT full:
+// place the player's token in the lowest available row in that column.
+
+// After each move, check for 4 matching tokens:
+// Horizontally
+// Vertically
+// Diagonally (both directions)
+// If the board does not have 4 matching tokens, continue to the next step.
+
+// Check for a Tie
+// If the board is not full, continue to the next step.
+// If the board is full and there is no winner, declare a tie.
+
+// Switch Turns
+// Change to the other player.
+// Repeat steps 2-7 until there is a winner.
+
+// End the Game
+// If there’s a winner, display the winner and end the game.
+// If it’s a tie, display a tie message.
+// Offer option to restart or exit.
 
 // Constants
 
 // Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
