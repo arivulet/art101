3c6fd64c9b93a81f5b22c76919d4fb29ca07e62e
River K <nightwingsking25@gmail.com>
2025-04-23 20:13:51 -0700

    lab5
    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,12 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+let make = 'Audi';
+let model = 'A3';
+let color = 'Black';
+let year = 2013;
 
-// Constants
+let age = 2025 - year;
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+document.writeln("Make: " + make + "<br>")
+document.writeln("Model: " + model + "<br>")
+document.writeln("Color: " + color + "<br>")
+document.writeln("Year: " + year + "<br>")
+document.writeln("Age: " + age + "y<br>")
