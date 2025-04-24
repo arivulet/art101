0562b3d210a453d1ec897eb83e89fca11a1e23b7
River K <nightwingsking25@gmail.com>
2025-04-23 23:56:54 -0700

    genious
    gebious

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -16,26 +16,18 @@
 // Update the board
 // If the chosen column is NOT full:
 // place the player's token in the lowest available row in that column.
-
 // After each move, check for 4 matching tokens:
 // Horizontally
 // Vertically
 // Diagonally (both directions)
 // If the board does not have 4 matching tokens, continue to the next step.
-
 // Check for a Tie
 // If the board is not full, continue to the next step.
 // If the board is full and there is no winner, declare a tie.
-
 // Switch Turns
 // Change to the other player.
 // Repeat steps 2-7 until there is a winner.
-
 // End the Game
 // If there’s a winner, display the winner and end the game.
 // If it’s a tie, display a tie message.
 // Offer option to restart or exit.
-
-// Constants
-
-// Functions
--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,12 +1,12 @@
-let make = 'Audi';
-let model = 'A3';
-let color = 'Black';
-let year = 2013;
+let make = "Adidas";
+let model = "Samba";
+let color = "Black";
+let year = 2024;
 
 let age = 2025 - year;
 
-document.writeln("Make: " + make + "<br>")
-document.writeln("Model: " + model + "<br>")
-document.writeln("Color: " + color + "<br>")
-document.writeln("Year: " + year + "<br>")
-document.writeln("Age: " + age + "y<br>")
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "y<br>");
