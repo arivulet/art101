11a749dea4b67a6317c3951b751a82a94ba2f8fa
River K <nightwingsking25@gmail.com>
2025-05-15 22:56:49 -0700

    lab10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,21 +1,40 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - print ozymandius
+// Author: River Kinley
+// Date: 14 May 2025
 
-// Constants
+// import poem, break into lines
+let ozy = [
+	"I met a traveller from an antique land,",
+	"Who said—Two vast and trunkless legs of stone",
+	"Stand in the desert. . . . Near them, on the sand,",
+	"Half sunk a shattered visage lies, whose frown,",
+	"And wrinkled lip, and sneer of cold command,",
+	"Tell that its sculptor well those passions read",
+	"Which yet survive, stamped on these lifeless things,",
+	"The hand that mocked them, and the heart that fed;",
+	"And on the pedestal, these words appear:",
+	"My name is Ozymandias, King of Kings;",
+	"Look on my Works, ye Mighty, and despair!",
+	"Nothing beside remains. Round the decay",
+	"Of that colossal Wreck, boundless and bare",
+	"The lone and level sands stretch far away.",
+];
 
-// Functions
+let currentLine = 0;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function printOzy() {
+	// display the current line
+	let curLin = ozy[currentLine];
+
+	// Move to the next line, looping back to 0 if at the end
+	currentLine = (currentLine + 1) % ozy.length;
+
+	return curLin;
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// on click run print ozy
+$("#talk").click(function () {
+	const Line = printOzy();
 
-// let's get this party started
-main();
+	$("#output").append('<div class="poem"><p>' + Line + "</p></div>");
+});
