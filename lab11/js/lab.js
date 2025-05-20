97fba4b6c608fb26a3c4014160d2dc0f510f83a8
River K <nightwingsking25@gmail.com>
2025-05-19 19:44:25 -0700

    lab11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,21 +1,48 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - create sort and anagram, and detect buttons to use dat jawns
+// Author: River Kinley
+// Date: 19 May 2025
 
-// Constants
+// on click of button, copy text, and sort
 
-// Functions
+// on click of button, copy text, and sort
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function sortString(inputString) {
+	// We have to convert our string to an array and back again to sort it
+	return inputString.split("").sort().join("");
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
+$("#confirm").click(function () {
+	const userName = $("#username").val();
+	userNameSorted = sortString(userName);
+	$("#output").html('<div class="text"><p>' + userNameSorted + "</p></div>");
+});
+
+// all credit to Wes Modes for this anagram function!!!!!!!!!! they credit chatgpt in their code but shoutout wes
+function anagram(inputStringa) {
+	const charArray = inputStringa.split("");
+
+	for (let i = charArray.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[charArray[i], charArray[j]] = [charArray[j], charArray[i]];
+	}
+
+	// Join the shuffled characters back into a string
+	const anagram = charArray.join("");
+
+	// Return the generated anagram
+	return anagram;
 }
 
-// let's get this party started
-main();
+$("#confirma").click(function () {
+	const auserName = $("#ausername").val();
+	userNameanagram = anagram(auserName);
+	$("#aoutput").html('<div class="text"><p>' + userNameanagram + "</p></div>");
+});
+
+// ALL BELOW ALSO CREDIT TO WES MODES
+// credit to ChatGPT
+String.prototype.toTitleCase = function () {
+	return this.replace(/\w\S*/g, function (txt) {
+		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+	});
+};
